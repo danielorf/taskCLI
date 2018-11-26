b026c73f62096060be194b05f90e9bb35992ff41
Daniel Orf <dorf@suse.com>
2018-11-25 22:15:41 -0800

    Added db directory, updated main.go

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/danielorf/task/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/danielorf/task/cmd"
+	"github.com/danielorf/task/db"
+	"github.com/mitchellh/go-homedir"
+)
 
 func main() {
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, "tasks.db")
+	must(db.Init(dbPath))
 	cmd.Execute()
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
